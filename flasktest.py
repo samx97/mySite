c0676ba97d4e202f33fda00b8edbd00612f80825
samx97 <dillydong1@gmail.com>
2018-02-03 10:54:22 +0600

    first commit

---
+++ flasktest.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    # app.run(host=os.getenv('IP', '0.0.0.0'),
+    #         port=int(os.getenv('PORT', 8888)), debug=True)
+    app.run(port=8080,debug=True)
